e29692ae31f110eddd554ee1ddb9430324fb0a94
Victor Borja <vborja@apache.org>
2012-08-03 17:52:32 -0500

    Use data-bind attribute when given string.

--- knockout-jqueryBindingProvider.js
+++ knockout-jqueryBindingProvider.js
@@ -1,6 +1,6 @@
 ;(function($, underlyingProvider, undefined){
 
-  var KO = 'ko-binding';
+  var KO = 'ko-binding', DATA = 'data-bind';
 
   var jqBindingProvider = function() {
 
@@ -24,21 +24,45 @@
   jqBindingProvider.prototype = underlyingProvider;
   ko.bindingProvider.instance = new jqBindingProvider;
 
-  $.fn.ko = function(value) {
-    var $this = $(this);
-    if(arguments.length == 2) {
-      var viewModel = arguments[0]
-        , bindings = arguments[1];
-      for (var selector in bindings) {
-        if(bindings.hasOwnProperty(selector)) {
-          $this.find(selector).data(KO, bindings[selector]);
+  var bindChilds = function($this, bindings) {
+    for (var selector in bindings) {
+      if(bindings.hasOwnProperty(selector)) {
+        var el, value = bindings[selector];
+        el = $this.find(selector);
+        if(el && el.length > 0) {
+          if(typeof value === 'string') {
+            el.attr(DATA, value);
+          } else {
+            $this.data(KO, value);
+          }
         }
       }
-      ko.applyBindings(viewModel, $this.get(0));
+    }
+  };
+
+  $.fn.ko = function(value) {
+    var $this = $(this);
+    if(arguments.length === 2) {
+      var viewModel = arguments[0]
+        , bindings = arguments[1];
+      if (typeof bindings === 'string') {
+        $this.attr(DATA, bindings);
+      } else {
+        bindChilds($this, bindings);
+      }
+      $this.each(function() {
+        ko.applyBindings(viewModel, this);
+      });
     } else {
-      $this.data(KO, value);
+      if(typeof value === 'string') {
+        $this.attr(DATA, value);
+      } else if(typeof value === 'object') {
+        bindChilds($this, value);
+      } else {
+        $this.data(KO, value);
+      }
     }
     return this;
   };
-  
+
 })(jQuery, ko.bindingProvider.instance)
